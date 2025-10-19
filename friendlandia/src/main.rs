3652d22e15462061d8b08829ca3fd6bbfaadec2d
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-10-19 16:15:55 -0600

    Update main.rs

--- friendlandia/src/main.rs
+++ friendlandia/src/main.rs
@@ -7,6 +7,7 @@ mod tcpclient;
 //TODO: Use thread::spawn for tcpserver
 //TODO: Moderation messages, message ids
 //TODO: Text writer w/messages, make a reader for it too
+//Decide about tokio
 fn main(){
     println!("Server or client? server/client");
     let mut a = String::new();
