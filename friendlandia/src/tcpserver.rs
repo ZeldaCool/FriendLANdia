ae11e42d8ecb4d0b5f85a75ffaba5c77d784efba
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-10-21 18:31:10 -0600

    added tokio-tungstenite as dependency

--- friendlandia/src/tcpserver.rs
+++ friendlandia/src/tcpserver.rs
@@ -1,24 +1,27 @@
 use tokio::net::TcpListener;
 use std::io;
 use std::error::Error;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-pub async fn handle_connection<T>(socket: T){
+
+pub async fn handle_connection<T>(stream: T){
     todo!();
 }
 #[tokio::main]
-pub async fn main() -> io::Result<()>  {
+pub async fn main() -> Result<(), Box<dyn Error>>  {
     let listener = TcpListener::bind("127.0.0.1:7878").await?;
 
     loop {
         match listener.accept().await {
-        Ok((socket, _)) => {
+        Ok((stream, _)) => {
             println!("Connection recieved.");
             tokio::spawn(async move {
-            handle_connection(socket).await
+            handle_connection(stream).await
         });
         },
         Err(e) => println!("couldn't get client: {:?}", e),
         }   
         
     }
+    Ok(())
 }
---
+++ friendlandia/src/usernameserver.rs
@@ -0,0 +1,4 @@
+//A username server that you can refrence to get private ips
+pub fn server_setup(){
+    println!("Hello, world!");
+}
