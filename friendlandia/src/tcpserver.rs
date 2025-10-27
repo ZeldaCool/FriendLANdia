86e4ac097a88340ca348e11155165eb4deec176d
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-10-27 16:00:50 -0600

    Basic message handler, adding MutEx and Arc to broadcast client messages

--- friendlandia/src/tcpserver.rs
+++ friendlandia/src/tcpserver.rs
@@ -2,20 +2,32 @@ use tokio::net::TcpListener;
 use std::io;
 use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::RwLock;
+use tokio::sync::Mutex;
+use std::sync::Arc;
+use tokio::task;
+//
 //Utilize tokio's mpsc to broadcast messages between tasks
-pub async fn handle_connection<T>(stream: T){
-    todo!();
-}
+
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>>  {
     let listener = TcpListener::bind("127.0.0.1:7878").await?;
-
+    let mut buffer = [0; 512];
     loop {
         match listener.accept().await {
-        Ok((stream, _)) => {
+        Ok((mut stream, _)) => {
             println!("Connection recieved.");
             tokio::spawn(async move {
-            handle_connection(stream).await
+                while let Ok(n) = stream.read(&mut buffer).await {
+                if n == 0 {
+                    break;
+                }
+                println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+                let response = b"Hello, client!";
+                stream.write_all(response).await.expect("Failed to write");
+            }
         });
         },
         Err(e) => println!("couldn't get client: {:?}", e),
